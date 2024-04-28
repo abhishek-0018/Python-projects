15daa264c5d8cfde4a97f5f25d9a77dccd57545a
abhishek-0018 <2k22cscys2210966@gmail.com>
2024-04-28 13:08:11 +0530

    Auction program

---
+++ Auction.py
@@ -0,0 +1,17 @@
+import os
+candidates={}
+highest_bidder=""
+highest_bid=0
+is_their_any_bidder="Yes"
+while is_their_any_bidder!="No":
+    name=input("Enter your name")
+    bid=int(input("Enter your bid"))
+    candidates[name]=bid
+    is_their_any_bidder=input("Are there more candidates in the room? Type Yes if any else No")
+    if is_their_any_bidder=="Yes":
+        os.system('cls')
+for i in candidates:
+    if candidates[i]>highest_bid:
+        highest_bid=candidates[i]
+        highest_bidder=i
+print(f"The highest bidder is {highest_bidder} who bidded ${highest_bid}")
